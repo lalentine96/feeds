020938e72bc31c2fde7d03502fb3adea338e0ab7
lalentine96 <53940679+lalentine96@users.noreply.github.com>
2020-05-19 12:01:00 +0300

    add greeting to the main page (#9)

--- src/components/pages/feeds-page.js
+++ src/components/pages/feeds-page.js
@@ -8,7 +8,7 @@ import { connect } from 'react-redux';
 import { withFeedsService } from '../hoc';
 import { logout } from '../../actions';
 
-const FeedsPage = ({ logout, isDemo, feedsService, history }) => {
+const FeedsPage = ({ logout, isDemo, login, feedsService, history }) => {
     const onReset = () => {
         feedsService.reset()
             .then(() => history.push('feeds'))
@@ -23,6 +23,13 @@ const FeedsPage = ({ logout, isDemo, feedsService, history }) => {
 
     return (
         <div className="page feeds-page">
+            <div className="greeting">
+                <h2>Hello {login}!</h2>
+                {
+                    isDemo &&
+                    <h5>You work with demo of the project</h5>
+                }
+            </div>
             <FeedsButtonPanel>
                 {buttons}
             </FeedsButtonPanel>
@@ -32,7 +39,7 @@ const FeedsPage = ({ logout, isDemo, feedsService, history }) => {
     );
 };
 
-const mapStateToProps = ({ isDemo }) => ({ isDemo });
+const mapStateToProps = ({ isDemo, login }) => ({ isDemo, login });
 
 const mapDispatchToProps = (dispatch, { feedsService }) => {
     return {
